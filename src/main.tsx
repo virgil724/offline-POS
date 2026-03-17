edf117e406e4106f8030cb3eb3276e29f0ed5225
TANG CHENG LEE <virgil246@gmail.com>
2026-03-17 17:01:00 +0800

    add  sentry for error record

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import * as Sentry from "@sentry/react";
 
-createRoot(document.getElementById('root')!).render(
+Sentry.init({
+  dsn: "https://df3633ee77f2a180602f0bcc664dda43@o4509485649756160.ingest.us.sentry.io/4511059007438848",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
